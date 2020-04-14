4491cf2592fd940714b6d0718275e3a777f18503
-l <fancyop@qq.com>
2020-04-14 15:59:08 +0800

    Update singly linked list test code

--- linked-list/singlylinkedlist.c
+++ linked-list/singlylinkedlist.c
@@ -25,20 +25,19 @@ int main(void)
 {
 	int n;
 
+	printf("-------------- Singly Lisked List Test! --------------\n");
+	printf("0 inset_begin()...\n");
+	printf("1 inset_last()...\n");
+	printf("2 inset_random()...\n");
+	printf("3 delete_begin()...\n");
+	printf("4 delete_last()...\n");
+	printf("5 delete_random()...\n");
+	printf("6 display()...\n");
+	printf("7 search()...\n");
+	printf("-1 exit...\n");
+	printf("------------------------------------------------------\n");
+
     while(1){
-		printf("-------------- Singly Lisked List Test! --------------\n");
-		printf("0 inset_begin()...\n");
-		printf("1 inset_last()...\n");
-		printf("2 inset_random()...\n");
-
-		printf("3 delete_begin()...\n");
-		printf("4 delete_last()...\n");
-		printf("5 delete_random()...\n");
-
-		printf("6 display()...\n");
-		printf("7 search()...\n");
-		printf("-1 exit...\n");
-		printf("------------------------------------------------------\n");
 		printf("Please enter test option value: ");
 		scanf("%d", &n);
     	switch (n){
@@ -73,7 +72,7 @@ void inset_begin(void)
 
 	ptr->next = head;
 	head = ptr;
-	printf("\n");
+	printf("Node inserted successfully\n\n");
 }
 
 void inset_last(void)
@@ -101,7 +100,7 @@ void inset_last(void)
 		temp->next = ptr;
 		ptr->next = NULL;
 	}
-	printf("\n");
+	printf("Node inserted successfully\n\n");
 }
 
 void inset_random(void)
@@ -118,36 +117,48 @@ void inset_random(void)
 	printf("Please enter the value(int) to random insert: ");
 	scanf("%d", &item);
 	ptr->data = item;
-	printf("Enter the location(>=0) of the node to perform the insertion after this node: ");
+	printf("Enter the location(>=0) of any node to insert: ");
 	scanf("%d", &loc);
 
-	if(head == NULL){
-		printf("Linked list cannot be empty\n\n");
-		return;
-	}
-	temp = head;
-	for(i = 0; i < loc; i++){
-		temp = temp->next;
-		if(temp == NULL){
+	if(head == NULL){	// loc can only be 0
+		if(loc == 0){
+			ptr->next = head;
+			head = ptr;
+		}else{
 			printf("No node can be inserted here\n\n");
 			return;
 		}
+	}else{
+		if(loc == 0){	// Need to change head
+			ptr->next = head;
+			head = ptr;
+		}else{
+			temp = head;
+			for(i = 1; i < loc; i++){
+				temp = temp->next;
+				if(temp == NULL){
+					printf("No node can be inserted here\n\n");
+					return;
+				}
+			}
+			ptr->next = temp->next;
+			temp->next = ptr;
+		}
 	}
-	ptr->next = temp->next;
-	temp->next = ptr;
-	printf("\n");
+	printf("Node inserted successfully\n\n");
 }
 
 void delete_begin(void)
 {
 	struct node *temp;
 	if(head == NULL){
-		printf("The linked list is empty and cannot be deleted\n");
+		printf("The linked list is empty and cannot be deleted\n\n");
 		return;
 	}
 	temp = head->next;
 	free(head);
 	head = temp;
+	printf("Node deleted successfully\n\n");
 }
 
 void delete_last(void)
@@ -155,50 +166,64 @@ void delete_last(void)
 	struct node *temp, *temp1;
 	if(head == NULL){
 		printf("The linked list is empty and cannot be deleted\n");
-		return;
 	}else if(head->next == NULL){
 		head = NULL;
 		free(head);
-		return;
+		printf("Node deleted successfully\n\n");
+	}else{
+		temp = head;
+		while(temp->next != NULL){
+			temp1 = temp;
+			temp = temp->next;
+		}
+		temp1->next = NULL;
+		free(temp);
+		printf("Node deleted successfully\n\n");
 	}
-	temp = head;
-	while(temp->next != NULL){
-		temp1 = temp;
-		temp = temp->next;
-	}
-	temp1->next = NULL;
-	free(temp);
 }
 
 void delete_random(void)
 {
 	struct node *temp, *temp1;
 	int loc, i;
-	printf("Please enter the node location(>=1) you want to delete: ");
-	scanf("%d", &loc);
-	temp = head;
-	for(i = 0; i < loc; i++){
-		if(temp->next == NULL){
-			printf("The location node cannot be deleted here\n");
-			return;
+
+	if(head == NULL){
+		printf("The linked list is empty and cannot be deleted\n");
+	}else{
+		printf("Please enter the node location(>=1) you want to delete: ");
+		scanf("%d", &loc);
+		if(loc == 0){
+			temp = head->next;
+			free(head);
+			head = temp;
+			printf("Node deleted successfully\n\n");
+		}else{
+			temp = head;
+			for(i = 0; i < loc; i++){
+				if(temp->next == NULL){
+					printf("The location node cannot be deleted here\n");
+					return;
+				}
+				temp1 = temp;
+				temp = temp->next;
+			}
+			temp1->next = temp->next;
+			free(temp);
+			printf("Node deleted successfully\n\n");
 		}
-		temp1 = temp;
-		temp = temp->next;
 	}
-	temp1->next = temp->next;
-	free(temp);
 }
 
 void display(void)
 {
 	int n=0;
 	struct node *temp;
-	temp = head;
 
-	printf("Display the entire singly linked list:\n");
+	printf("Display the entire linked list:\n");
 	if(head == NULL){
 		printf("Linked list is empty\n");
 	}
+	temp = head;
 	while(temp != NULL){
 		printf("-%02d- %d\n", n++, temp->data);
 		temp = temp->next;
@@ -209,7 +234,7 @@ void display(void)
 void search(void)
 {
 	struct node *temp;
-	int item, loc, flag;
+	int item, loc, flag=0;
 	printf("Please enter the value you want to search: ");
 	scanf("%d",&item);
 	if(head == NULL){
@@ -220,7 +245,7 @@ void search(void)
 	loc = 0;
 	while(temp != NULL){
 		if(temp->data == item){
-			printf("Find the target item at location %d", loc);
+			printf("Find the target item at location %d\n", loc);
 			flag = 1;
 		}
 		temp = temp->next;
